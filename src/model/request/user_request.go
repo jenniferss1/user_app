1cbde70b118b21b344c935405fbc69f858522562
jennifer <jennifersantossouza@hotmail.com>
2026-01-20 17:48:38 -0300

    feat: validating the user request fields

--- src/model/request/user_request.go
+++ src/model/request/user_request.go
@@ -1,10 +1,10 @@
 package request
 
 type UserRequest struct {
-	Email    string  `json:"email"`
-	Password string  `json:"password"`
-	Name     string  `json:"name"`
-	Age      int8    `json:"age"`
-	Weight   float64 `json:"weight"`
-	Height   float64 `json:"height"`
+	Email    string  `json:"email" binding:"required,email"`
+	Password string  `json:"password" binding:"required,min=8,containsany=!@#$%&*"`
+	Name     string  `json:"name" binding:"required,min=4,max=50"`
+	Age      int8    `json:"age" binding:"required,min=18,max=100"`
+	Weight   float64 `json:"weight" binding:"required"`
+	Height   float64 `json:"height" binding:"required"`
 }
